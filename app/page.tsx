37eb5d5b540cf2790a9ca19955af6a83a26335b0
Syed Javeed <syedjaveed@nextgen-is.com>
2024-09-03 00:33:34 +0530

    add dark-mode toogle

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import ClientProviders from "@/components/client-prover";
 import LandingPage from "@/pages/LandingPage";
 
 export default function Home() {
@@ -5,7 +6,9 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-[var(--background)]">
+      <ClientProviders >
       <LandingPage />
+      </ClientProviders>
     </main>
   );
 }
